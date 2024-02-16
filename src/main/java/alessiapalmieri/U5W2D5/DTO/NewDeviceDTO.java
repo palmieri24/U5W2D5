379b0d0a8d57749b548d63056a53e2b00ba20771
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-02-16 12:04:09 +0100

    Added DTOs and My Diagram Idea

--- src/main/java/alessiapalmieri/U5W2D5/DTO/NewDeviceDTO.java
+++ src/main/java/alessiapalmieri/U5W2D5/DTO/NewDeviceDTO.java
@@ -1,4 +1,9 @@
 package alessiapalmieri.U5W2D5.DTO;
 
-public record NewDeviceDTO() {
+import jakarta.validation.constraints.NotEmpty;
+
+public record NewDeviceDTO(
+        @NotEmpty(message = "Device type is a mandatory field! Choose between SMARTPHONE, TABLET, LAPTOP")
+        String deviceType
+) {
 }
--- src/main/java/alessiapalmieri/U5W2D5/DTO/NewEmployeeDTO.java
+++ src/main/java/alessiapalmieri/U5W2D5/DTO/NewEmployeeDTO.java
@@ -1,4 +1,23 @@
 package alessiapalmieri.U5W2D5.DTO;
 
-public record NewEmployeeDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record NewEmployeeDTO(
+        @NotEmpty
+        @Size(min = 3,max = 20, message = "The username must be a minimum of 3 characters and a maximum of 20")
+        String username,
+        @NotEmpty(message = "enter name")
+        @Size(min = 3,max = 20, message = "The name must be a minimum of 3 characters and a maximum of 20")
+        String name,
+        @NotEmpty(message = "Enter lastname")
+        @Size(min = 3,max = 20, message = "The lastname must be a minimum of 3 characters and a maximum of 20")
+        String surname,
+        @Email(message = "Enter email")
+        @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Email is not valid!")
+        String email,
+        @NotEmpty(message = "Enter Avatar URL")
+        String avatarUrl) {
 }
